4677a21756e4bf67978a3ab1e50fddda9d4e5246
KennethKcc98 <44659208+KennethKcc98@users.noreply.github.com>
2018-11-26 18:15:29 +0800

    Add files via upload

---
+++ Week 1/Fibonacci.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class Fibonacci{
+    public static long fibonacci(int n,int t1,int t2){
+        for (int i = 1; i <= n; ++i)
+        {
+            System.out.print(t1 + " ");
+            int sum = t1 + t2;
+            t1 = t2;
+            t2 = sum;
+        }
+        return t1;
+    }
+    public static void main(String [] args){
+        Scanner s = new Scanner(System.in);
+        try{
+            System.out.print("Number you want to print:");
+            int n= s.nextInt();
+            if(n<=0){
+                throw new Exception("number should not less or equal 0");
+            }
+            System.out.print(fibonacci(n, 1,1));
+
+        }catch(InputMismatchException ex){
+            System.out.print("Please input number!");
+        }
+        catch(Exception ex){
+            System.out.print(ex.getMessage());
+        }
+    }
+}
